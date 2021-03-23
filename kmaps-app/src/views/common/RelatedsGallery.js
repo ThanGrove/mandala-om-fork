2073afa93c705360501c6750bdae81272d132e10
ThanGrove <than@virginia.edu>
2021-03-23 17:32:57 -0400

    update to gallery view mode paths for consistency

--- kmaps-app/src/views/common/RelatedsGallery.js
+++ kmaps-app/src/views/common/RelatedsGallery.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useRouteMatch, Redirect } from 'react-router-dom';
 import { FeatureCollection } from './FeatureCollection';
 import { useKmapRelated } from '../../hooks/useKmapRelated';
 import { useUnPackedMemoized } from '../../hooks/utils';
@@ -7,9 +7,9 @@ import { queryID } from '../../views/common/utils';
 
 // Special case of the FeatureGallery
 export default function RelatedsGallery({ baseType }) {
-    let { id, relatedType: type, definitionID } = useParams(); // USES PARAMS from React Router  Refactor?
+    let { id, relatedType: type, definitionID, viewMode } = useParams(); // USES PARAMS from React Router  Refactor?
     definitionID = definitionID ?? 'noDefID';
-
+    let { path } = useRouteMatch();
     const [perPage, setPerPage] = useState(100);
     const [page, setPage] = useState(0); // Start will always be page * perPage
     const {
@@ -44,14 +44,31 @@ export default function RelatedsGallery({ baseType }) {
     const allAssets = kmapsRelated?.assets;
     const assets = allAssets ? allAssets[type] : null;
     const docs = assets?.docs;
-
+    if (!viewMode) {
+        const listTypes = ['places', 'sources', 'subjects', 'texts'];
+        let calcViewMode = 'deck';
+        if (listTypes.includes(type)) {
+            calcViewMode = 'list';
+        } else if (type === 'images') {
+            calcViewMode = 'gallery';
+        }
+        let newpath = path
+            .replace(':id', id)
+            .replace(':relatedType', type)
+            .replace(':viewMode', calcViewMode);
+        // When path doesn't have a view mode, use default (calc)
+        if (!newpath.includes(calcViewMode)) {
+            newpath += '/' + calcViewMode;
+        }
+        return <Redirect to={newpath} />;
+    }
     // Give a nice title.
     const title = type !== 'all' ? `Related ${type}` : 'All Related Items';
     return (
         <FeatureCollection
             docs={docs}
             title={title}
-            viewMode={'gallery'}
+            viewMode={viewMode}
             inline={true}
             page={page}
             setPage={setPage}
