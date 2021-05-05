0da2e7ae23b201198c33665a5fffee5008669a91
ThanGrove <than@virginia.edu>
2021-05-05 12:30:25 -0400

    Adding perspective data file

---
+++ kmaps-app/src/views/KmapTree/KmapPerspectives.js
@@ -0,0 +1,23 @@
+export const KmapPerpsectiveData = {
+    places: [
+        { name: 'Cultural Regions', id: 'cult.reg' },
+        { name: 'National Administrative Units', id: 'pol.admin.hier' },
+        {
+            name: 'Historical Polity Administrative Units',
+            id: 'hist.pol.admin.unit',
+        },
+        { name: 'Electoral Relationships', id: 'elect.rel' },
+        { name: 'Site Relationships', id: 'site.rel' },
+        { name: 'Political Relationships', id: 'pol.rel' },
+        { name: 'Cultural Relationships', id: 'cult.rel' },
+        { name: 'Environmental Relationships', id: 'envir.rel' },
+        { name: 'Administrative Relationships', id: 'admin.rel' },
+        { name: 'Organizational Relationships', id: 'org.rel' },
+        { name: 'Religious Relationships', id: 'rel.rel' },
+        { name: 'Geographic Relationship', id: 'geo.rel' },
+    ],
+
+    subjects: [],
+
+    terms: [],
+};
