d5977c80782ac3cb18b439e145ed4b29e2cbf951
ThanGrove <than@virginia.edu>
2021-08-04 11:38:08 -0400

    Update to useSolr to allow filtering when boolen is true and adding gallery of collections by project at collections home

--- kmaps-app/src/views/Collections/CollectionsHome.js
+++ kmaps-app/src/views/Collections/CollectionsHome.js
@@ -1,15 +1,50 @@
 import React, { useEffect } from 'react';
-import useStatus from '../../hooks/useStatus';
+import { useSolr } from '../../hooks/useSolr';
+import MandalaSkeleton from '../common/MandalaSkeleton';
+import { FeatureCollection } from '../common/FeatureCollection';
+import { getProject } from '../common/utils';
+import { SAProjectName } from '../common/utilcomponents';
 
 export function CollectionsHome(props) {
-    const status = useStatus();
+    const querySpecs = {
+        index: 'assets',
+        params: {
+            q: `asset_type:collections`,
+            sort: 'title_sort_s asc',
+            rows: 1000,
+        },
+    };
+    const {
+        isLoading: isCollsLoading,
+        data: collsData,
+        isError: isCollsError,
+        error: collsError,
+    } = useSolr('all-collections', querySpecs, false, true);
 
-    useEffect(() => {
-        status.clear();
-        status.setHeaderTitle('Collections Home');
-    });
+    let mscope = 'all of Mandala';
+    const current_project = getProject();
+    if (current_project) {
+        mscope = <SAProjectName pid={current_project} />;
+    }
 
-    return <> Collections Home: Not much here yet.. </>;
+    if (isCollsLoading) {
+        return <MandalaSkeleton />;
+    }
+
+    if (isCollsError) {
+        console.log('Error loading all collections: ', collsError);
+    }
+
+    return (
+        <div>
+            <h1>All Collections</h1>
+            <p>
+                This page shows all the asset collections and subcollections for
+                the {mscope} project:
+            </p>
+            <FeatureCollection docs={collsData.docs} />
+        </div>
+    );
 }
 
 export default CollectionsHome;
