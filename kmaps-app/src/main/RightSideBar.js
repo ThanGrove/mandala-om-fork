8f959babde166d8c8a4373b217a1a82b612f7b82
ThanGrove <than@virginia.edu>
2021-09-21 16:00:10 -0400

    Update dependency for right sidebar

--- kmaps-app/src/main/RightSideBar.js
+++ kmaps-app/src/main/RightSideBar.js
@@ -46,7 +46,7 @@ export default function RightSideBar() {
             }
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [match.url]);
+    }, [match?.url]);
 
     const advancedSearchPortal = (
         <Section
