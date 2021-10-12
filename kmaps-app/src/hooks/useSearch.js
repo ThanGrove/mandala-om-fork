bfaaf998d7792d5fad0a3cdf2e8e6897c1bbc299
ThanGrove <than@virginia.edu>
2021-10-12 09:24:25 -0400

    adding filter for subjects-9667, english letters, to filter query in searches

--- kmaps-app/src/hooks/useSearch.js
+++ kmaps-app/src/hooks/useSearch.js
@@ -232,9 +232,11 @@ function constructTextQuery(searchString) {
 function constructFilters(filters) {
     // If no filters are passed then we return the all the assets.
     if (_.isEmpty(filters)) {
+        // filter out grouping terms (letters, 9311, and phrases, 9314, 9667 - English letters) for terms trees,
+        const xrelated = ['subjects-9311', 'subjects-9314', 'subjects-9667'];
         const fqs = [
             'asset_type:(audio-video images texts visuals sources subjects places terms)', // filter out old unused asset types
-            '-related_uid_ss:(subjects-9311 OR subjects-9314)', // filter out grouping terms (letters, 9311, and phrases, 9314) for terms trees
+            '-related_uid_ss:(' + xrelated.join(' OR ') + ')',
         ];
         // Added by Than for project filtering
         const projid = getProject();
